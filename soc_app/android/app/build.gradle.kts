f99a5f2d3a58ec891882e0a5847c8ecf9b95b0c1
SOC Assessment <admin@soc-assessment.dev>
2026-06-25 00:32:27 +0800

    fix: Android compileSdk 36, overridePathCheck, proxy config for build

--- soc_app/android/app/build.gradle.kts
+++ soc_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.soc.soc_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
